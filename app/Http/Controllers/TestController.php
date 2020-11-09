f73a92749dbfa7ae1f061b8a14a8bf75fdd118a2
caoyufei <3183989523@qq.com>
2020-11-09 16:31:02 +0800

    1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,35 +9,6 @@ use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
-    //微信接入
-    public  function wx()
-    {
-        $echostr=request()->get('echostr','');
-        if($this->checkSignature && !empty($echostr)){
-            echo $echostr;
-        }
-    }
-
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = 'wx';
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-}
-
-
 //推送事件
 public function wxEvent()
 {
@@ -50,10 +21,12 @@ public function wxEvent()
     sort($tmpArr, SORT_STRING);
     $tmpStr = implode( $tmpArr );
     $tmpStr = sha1( $tmpStr );
-
+    $echostr=request()->get('echostr','');
+    if(!empty($echostr)){
+        echo $echostr;die;
+    }
     if( $tmpStr == $signature ){
        //验证通过
-        $this->wx();
         //1.接受数据
         $xml_data=file_get_contents("php://input");
 
